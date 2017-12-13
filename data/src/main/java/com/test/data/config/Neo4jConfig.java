d569aa8b6fedf972ee9a72309f6b9f1e789b34b6
452442066@qq.com <452442066@qq.com>
2017-12-13 18:15:07 +0800

    Initial commit

---
+++ data/src/main/java/com/test/data/config/Neo4jConfig.java
@@ -0,0 +1,18 @@
+package com.test.data.config;
+
+import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableNeo4jRepositories(basePackages = {"com.test.data.repositories"})
+public class Neo4jConfig extends Neo4jConfiguration {
+
+    @Override
+    public SessionFactory getSessionFactory() {
+        return new SessionFactory("com.test.data.domain");
+    }
+}
